37983f68cc7be2af0be889a518c23f5e3f892f27
Evanice4 <n.karabaran@alustudent.com>
2025-11-01 14:27:14 +0200

    fix: mask password input field

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 # Database connection
 db = mysql.connector.connect(
     host="localhost",
-    user="root",          # change if you use another username
-    password="Karabarang@Eva123",
+    user="root",          
+    password="****************",
     database="heart_diseases_db"
 )
 
